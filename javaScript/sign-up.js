8345372d492577f815e312a486ef202dd2655d3a
Chhan Ratana <chhanratta92000@gmail.com>
2024-02-01 22:10:55 +0700

    link-javascript-folder

--- javaScript/sign-up.js
+++ javaScript/sign-up.js
@@ -12,7 +12,7 @@ function change1(){
 const singn1=document.getElementById('sign_in');
 singn1.addEventListener('click',(e)=>{
     e.preventDefault();
-    window.location.href="sign-in.html"
+    window.location.href="../html/sign-in.html"
 })
 
 const email=document.getElementById('email');
@@ -37,7 +37,7 @@ sign_up_button.addEventListener('click',(e)=>{
     localStorage.setItem('first-name',firstName.value);
     localStorage.setItem('last-name',lastName.value);
     localStorage.setItem('address',_address.value);
-    window.location.href="authentication.html"
+    window.location.href="../html/authentication.html"
 })
 
 
